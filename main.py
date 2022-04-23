668c1fd592dcfbfe239f5f036111938d790dc898
Alexander Berryhill <amsb99@gmail.com>
2022-04-23 16:19:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from turtle import width
 import altair as alt
 import pandas as pd
 
 data = pd.read_csv("data.csv")
 
 iris_chart = alt.Chart(data, title="title").mark_circle().encode(
-    alt.X("x", title="x"),
-    alt.Y("y", title="y"),
-    alt.Color("Species")
+    alt.X("cty", title="x"),
+    alt.Y("hwy", title="y"),
+    alt.Color("manufacturer")
 ).properties(width=400, height=300)
 
 iris_chart
